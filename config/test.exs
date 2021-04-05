b47145f4f694d35f832c1ae9188393004481f426
Eric Kim-Butler <eric@kimbutler.xyz>
2021-04-04 22:50:08 -0400

    Add CI

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ config :kbf, Kbf.Repo,
   username: "postgres",
   password: "postgres",
   database: "kbf_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "postgres",
+  hostname: System.get_env("DB_HOST") || "postgres",
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :kbf, KbfWeb.Endpoint,
