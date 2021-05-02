f3eb6369f338c82b8233d763a8855346ac115455
Eric Kim-Butler <eric@kimbutler.xyz>
2021-05-02 19:40:59 -0400

    Fix Date Format

--- lib/kbf_web/format.ex
+++ lib/kbf_web/format.ex
@@ -11,7 +11,6 @@ defmodule KbfWeb.Format do
   def format_date(nil), do: ""
 
   def format_date(date) do
-    day_of_month = Number.Human.number_to_ordinal(date.day)
-    Calendar.strftime(date, "%b, #{day_of_month} %Y")
+    Calendar.strftime(date, "%d %b %Y")
   end
 end
